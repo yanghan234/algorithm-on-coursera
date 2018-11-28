785455b013be8794e0b26cb440cdd7e0e0e6eae4
Han Yang <yanghan234@outlook.com>
2018-11-28 15:47:24 -0600

    Implement percolation again with C++

--- Assignment1/Percolation.cpp
+++ Assignment1/Percolation.cpp
@@ -1,70 +1,186 @@
 #include <iostream>
 #include <exception>
+#include <random>
+#include <time.h>
 #include "Percolation.h"
 using namespace std;
 
 Percolation::Percolation( int num )
 {
-    if ( num <= 0 )
-        throw out_of_range("OutOfRangeException: N must be > 0!!");
-    _N = num;
-    _status.reserve(_N*_N);
-    _id.reserve(_N*_N+1);
-    for ( int i = 0; i < _N*_N; i++ )
-        _status[i] = 0;
-    for ( int i = 0; i < _N*_N+1; i++ )
-        _id[i] = i;
+  if ( num <= 0 )
+    throw out_of_range("OutOfRangeException: N must be > 0!!");
+  _N = num;
+  _numOfOpenSites = 0;
+  _status.reserve(_N*_N);
+  _id.reserve(_N*_N+1);
+  for ( int i = 0; i < _N*_N; i++ )
+    _status[i] = 0;
+  for ( int i = 0; i < _N*_N+1; i++ )
+    _id[i] = i;
 }
 
 bool Percolation::open( int row, int col )
 {
-    if ( row <= 0 || row > _N || col <= 0 || col > _N )
-        throw out_of_range("OutOfRangeException: row and col must be within [1,N]!!");
-    if ( isOpen( row, col ) )
+  if ( row <= 0 || row > _N || col <= 0 || col > _N )
+    throw out_of_range("OutOfRangeException: row and col must be within [1,N]!!");
+  if ( isOpen( row, col ) )
+  {
+    //cout << " Block ( " << row << ", "<<col<<" ) is already open" << endl;
+    return false;
+  }
+  int index, index2;
+  index = ( row - 1 ) * _N + ( col - 1 );
+  _status[index] = 1;
+  //cout << "Opening " << row << ", " << col << ", index = " << index << endl;
+  if ( row == 1 )
+    _id[index+1] = 0;
+  for ( int irow = row - 1; irow <= row+1; irow++ )
+  {
+    for ( int icol = col - 1; icol <= col+1; icol++ )
     {
-        cout << " Block ( " << row << ", "<<col<<" ) is already open" << endl;
-        return false;
+      index2 = ( irow - 1 ) * _N + ( icol - 1 );
+      if ( (row-irow)*(row-irow)+(col-icol)*(col-icol)==1
+        && check_integrity( irow, icol ) && isOpen(irow,icol) )
+      {
+        connect(index+1,index2+1);
+      }
     }
-
-    int index, index2;
-    if ( row == 1 )
-    {
-        index = ( row - 1 ) * _N + ( col - 1 );
-        _id[index+1] = 0;
-    }
-    for ( int irow = row - 1; irow <= row+1; irow++ )
-    {
-        for ( int icol = col - 1; icol <= col+1; icol++ )
-        {
-            if ( (row-irow)*(row-irow)+(col-icol)*(col-icol)==1
-                && check_integrity( irow, icol ) )
-            {
-                index2 = ( irow - 1 ) * _N + ( icol - 1 );
-                connect(index,index2);
-            }
-        }
-    }
-    return true;
+  }
+  _numOfOpenSites++;
+  return true;
 }
 
 bool Percolation::isOpen( int row, int col )
 {
-    if ( row <= 0 || row > _N || col <= 0 || col > _N )
-        throw out_of_range("OutOfRangeException: row and col must be within [1,N]!!");
-    int index = row * _N + col;
-    return _status[index];
+  if ( row <= 0 || row > _N || col <= 0 || col > _N )
+    throw out_of_range("OutOfRangeException: row and col must be within [1,N]!!");
+  int index = ( row - 1 ) * _N + ( col - 1 );
+  return _status[index]==1;
+}
+
+bool Percolation::isFull( int row, int col )
+{
+  int index = ( row - 1 ) * _N + col - 1;
+  return root(index+1) == 0;
+}
+
+bool Percolation::isPercolate()
+{
+  int row = _N;
+  for ( int col = 1; col <=_N; col++ )
+  {
+    int index = ( row - 1 ) * _N + ( col - 1);
+    if ( root(index+1) == 0 )
+      return true;
+  }
+  return false;
+}
+
+
+bool Percolation::check_integrity( int row, int col )
+{
+  if ( row <= 0 || row > _N || col <= 0 || col > _N )
+    return false;
+  else
+    return true;
+}
+
+void Percolation::connect( int p, int q )
+{
+  int rootp = root(p);
+  int rootq = root(q);
+  if ( rootp < rootq )
+    _id[rootq] = rootp;
+  else
+    _id[rootp] = rootq;
+}
+
+int Percolation::root( int p )
+{
+  int rootp = p;
+  while ( rootp != _id[rootp] )
+    rootp = _id[rootp];
+
+  while ( p != rootp )
+  {
+    int newp = _id[p];
+    _id[p] = rootp;
+    p = newp;
+  }
+
+  return rootp;
+}
+
+ostream& operator<<( ostream &os, const Percolation &rhs )
+{
+  os << "ID matrix: ";
+  for ( int row = 1; row <= rhs._N; row++ )
+  {
+    os << endl;
+    for ( int col = 1; col <= rhs._N; col++ )
+    {
+      int index = ( row - 1 ) * rhs._N + ( col - 1 );
+      os << rhs._id[index+1] << "\t";
+    }
+  }
+  os << endl;
+  os << "Status matrix:";
+  for ( int row = 1; row <= rhs._N; row++ )
+  {
+    os << endl;
+    for ( int col = 1; col <= rhs._N; col++ )
+    {
+      int index = ( row - 1 ) * rhs._N + ( col - 1 );
+      os << rhs._status[index] << "\t";
+    }
+  }
+  return os;
 }
 
 int main(int argc, char** argv)
 {
-    int N = atoi( argv[1] );
-    cout << N << endl;
-    try {
-        Percolation prc(N);
-        cout << prc.isOpen(1,-1);
+  if ( argc <= 1 )
+    throw out_of_range("No input parameter!!");
+  int N = atoi( argv[1] );
+  int nTests = atoi( argv[2] );
+  double ratios[nTests];
+  try {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(1,N);
+
+    for ( int itest = 0; itest < nTests; itest++ )
+    {
+      Percolation *prc = new Percolation(N);
+      while ( !prc->isPercolate() )
+      {
+        int row = dis(gen);
+        int col = dis(gen);
+        prc->open(row,col);
+      }
+      ratios[itest] = prc->numOfOpenSites()*1.0/(N*N);
+      // cout << *prc << endl;
+      // cout << prc->numOfOpenSites()*1.0/(N*N) << endl;
+      // cout << "Percolate? : " << prc->isPercolate() << endl;
     }
-    catch ( exception &e ) {
-        cout << e.what() << endl;
-    }
+
+    double mean = 0.0, stddev = 0.0;
+    for ( int itest = 0; itest < nTests; itest++ )
+      mean += ratios[itest];
+    mean = mean/nTests;
+
+    for ( int itest = 0; itest < nTests; itest++ )
+      stddev += (ratios[itest]-mean)*(ratios[itest]-mean);
+    stddev = sqrt(stddev/(nTests-1));
+
+    cout << "Average: " << mean << endl;
+    cout << "Std Dev: " << stddev << endl;
+    cout << "95%    : [" << mean-stddev*1.96/sqrt(nTests);
+    cout <<", "<<mean+stddev*1.96/sqrt(nTests) << "]"<<endl;
+
+  }
+  catch ( exception &e ) {
+    cout << e.what() << endl;
+  }
 
 }
--- Assignment1/Percolation.h
+++ Assignment1/Percolation.h
@@ -4,16 +4,20 @@ using namespace std;
 
 class Percolation
 {
-    public:
-        Percolation( int num );
-        bool open( int row, int col );
-        bool isOpen( int row, int col );
+  friend ostream& operator<<(ostream &os, const Percolation &rhs);
+  public:
+    Percolation( int num );
+    bool open( int row, int col );
+    bool isOpen( int row, int col );
+    bool isFull( int row, int col );
+    bool isPercolate();
+    int  numOfOpenSites() const { return _numOfOpenSites; };
 
-    private:
-        int _N;
-        vector<int> _status;
-        vector<int> _id;
-        bool check_integrity( int row, int col );
-        void connect( int p, int q );
-        int  root( int p );
+  private:
+    int _N,_numOfOpenSites;
+    vector<int> _status;
+    vector<int> _id;
+    bool check_integrity( int row, int col );
+    void connect( int p, int q );
+    int  root( int p );
 };
